de81ebc42d5c481f45bddf9f8f00654e2d3e5129
Thanaphoom Babparn <tp.babparn@gmail.com>
2025-01-03 21:48:11 +0900

    Start contributing Kotlin

---
+++ kotlin/src/main/kotlin/common/twopointers/ispalindrome/IsPalindromeValid.kt
@@ -0,0 +1,23 @@
+package common.twopointers.ispalindrome
+
+class IsPalindromeValid {
+
+    fun solution(s: String): Boolean {
+        var left = 0
+        var right = s.length - 1
+        while (left < right) {
+            while (left < right && s[left].isLetterOrDigit()) {
+                left++
+            }
+            while (left < right && s[right].isLetterOrDigit()) {
+                right--
+            }
+            if (s[left] != s[right]) {
+                return false
+            }
+            left++
+            right--
+        }
+        return true
+    }
+}
