af4f9f8e52f9e157e30b5e5a34c9cb7f7165bd06
Shun Ping Ong <ongshunping@hotmail.com>
2025-01-11 17:07:33 +0800

    Add C++ solutions for Chapter 4 (Fast and Slow Pointers)

---
+++ cpp/Fast and Slow Pointers/linked_list_midpoint.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition of ListNode:
+ * struct ListNode {
+ *     int val;
+ *     ListNode* next;
+ *     ListNode(int val = 0, ListNode* next = nullptr) : val(val), next(next) {}
+ * };
+ */
+
+ListNode* linkedListMidpoint(ListNode* head) {
+    ListNode* slow = head;
+    ListNode* fast = head;
+    // When the fast pointer reaches the end of the list, the slow
+    // pointer will be at the midpoint of the linked list.
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
