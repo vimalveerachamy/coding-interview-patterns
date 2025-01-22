56c8adea4ab71b095850cce0933cd7b35451261a
Shun Ping Ong <ongshunping@hotmail.com>
2025-01-22 22:43:55 +0800

    feat(cpp): add matrix_pathways_optimized.cpp

---
+++ cpp/Dynamic Programming/matrix_pathways_optimized.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+int matrixPathwaysOptimized(int m, int n) {
+    // Initialize 'prevRow' as the DP values of row 0, which are all 1s.
+    std::vector<int> prevRow(n, 1);
+    // Iterate through the matrix starting from row 1.
+    for (int r = 1; r < m; r++) {
+        // Set the first cell of 'currRow' to 1. This is done by 
+        // setting the entire row to 1.
+        std::vector<int> currRow(n, 1);
+        for (int c = 1; c < n; c++) {
+            // The number of unique paths to the current cell is the sum 
+            // of the paths from the cell above it ('prevRow[c]') and 
+            // the cell to the left ('currRow[c - 1]').
+            currRow[c] = prevRow[c] + currRow[c - 1];
+        }
+        // Update 'prevRow' with 'currRow' values for the next 
+        // iteration.
+        prevRow = currRow;
+    }
+    // The last element in 'prevRow' stores the result for 
+    // the bottom-right cell.
+    return prevRow[n - 1];
+}
