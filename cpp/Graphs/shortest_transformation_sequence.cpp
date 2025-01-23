bd5885991d21202cbe17cb2858beac351d404061
Shun Ping Ong <ongshunping@hotmail.com>
2025-01-23 08:48:06 +0800

    feat(cpp): add shortest_transformation_sequence.cpp

---
+++ cpp/Graphs/shortest_transformation_sequence.cpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <vector>
+#include <deque>
+#include <unordered_set>
+
+int shortestTransformationSequence(std::string& start, std::string& end, std::vector<std::string>& dictionary) {
+    std::unordered_set<std::string> dictionarySet(dictionary.begin(), dictionary.end());
+    if (dictionarySet.find(start) == dictionarySet.end() || dictionarySet.find(end) == dictionarySet.end()) {
+        return 0;
+    }
+    if (start == end) {
+        return 1;
+    }
+    std::string lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+    std::deque<std::string> queue;
+    queue.push_back(start);
+    std::unordered_set<std::string> visited;
+    visited.insert(start);
+    int dist = 0;
+    // Use level-order traversal to find the shortest path from the 
+    // start word to the end word.
+    while (!queue.empty()) {
+        int size = queue.size();
+        for (int unused = 0; unused < size; unused++) {
+            std::string currWord = queue.front();
+            queue.pop_front();
+            // If we found the end word, we've reached it via the 
+            // shortest path.
+            if (currWord == end) {
+                return dist + 1;
+            }
+            // Generate all possible words that have a one-letter difference 
+            // to the current word.
+            for (int i = 0; i < currWord.length(); i++) {
+                for (char c : lowerCaseAlphabet) {
+                    std::string nextWord = currWord;
+                    nextWord[i] = c;
+                    // If 'nextWord' exists in the dictionary, it's a 
+                    // neighbor of the current word. If unvisited, add it 
+                    // to the queue to be processed in the next level.
+                    if (dictionarySet.find(nextWord) != dictionarySet.end() && visited.find(nextWord) == visited.end()) {
+                        visited.insert(nextWord);
+                        queue.push_back(nextWord);
+                    }
+                }
+            }
+        }
+        dist++;
+    }
+    // If there is no way to reach the end word, then no path exists.
+    return 0;
+}
