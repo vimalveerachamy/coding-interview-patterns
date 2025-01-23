7be8452cc2fa65b139689d9beb3389d84b7d2fc0
Thanaphoom Babparn <tp.babparn@gmail.com>
2025-01-23 16:38:46 +0700

    Update method to use suffix helper on climbing stairs

--- kotlin/Dynamic Programming/ClimbingStairsTopDown.kt
+++ kotlin/Dynamic Programming/ClimbingStairsTopDown.kt
@@ -1,9 +1,9 @@
 fun climbingStairsTopDown(n: Int): Int {
     val memo = hashMapOf<Int, Int>()
-    return climbingStairsTopDown(n, memo)
+    return climbingStairsTopDownHelper(n, memo)
 }
 
-fun climbingStairsTopDown(n: Int, memo: HashMap<Int, Int>): Int {
+fun climbingStairsTopDownHelper(n: Int, memo: HashMap<Int, Int>): Int {
     // Base cases: With a 1-step staircase, there's only one way to
     // climb it. With a 2-step staircase, there are two ways to climb it.
     if (n <= 2) {
@@ -14,6 +14,6 @@ fun climbingStairsTopDown(n: Int, memo: HashMap<Int, Int>): Int {
     }
     // The number of ways to climb to the n-th step is equal to the sum
     // of the number of ways to climb to step n - 1 and to n - 2.
-    memo[n] = climbingStairsTopDown(n - 1, memo) + climbingStairsTopDown(n - 2, memo)
+    memo[n] = climbingStairsTopDownHelper(n - 1, memo) + climbingStairsTopDownHelper(n - 2, memo)
     return memo[n]!!
 }
