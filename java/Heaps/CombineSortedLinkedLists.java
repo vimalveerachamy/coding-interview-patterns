2b82cf1c1d668a3ed184bc883c4813213f82ad42
Jer3myYu <jeremyyucareer@gmail.com>
2025-01-23 21:06:20 -0500

    add: CombineSortedLinkedLists

---
+++ java/Heaps/CombineSortedLinkedLists.java
@@ -0,0 +1,54 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+import DS.ListNode;
+
+/*
+    // Definition of ListNode:
+    class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int val) { this.val = val; }
+        public ListNode(int val, ListNode next) {
+            this(val);
+            this.next = next;
+        }
+    }
+ */
+
+public class CombineSortedLinkedLists {
+    public ListNode combineSortedLinkedLists(List<ListNode> lists) {
+        // Define a custom comparator for 'ListNode', enabling the min-heap 
+        // to prioritize nodes with smaller values.
+        PriorityQueue<ListNode> heap = new PriorityQueue<>(new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode self, ListNode other) {
+                return Integer.compare(self.val, other.val);
+            }
+        });
+        // Push the head of each linked list into the heap.
+        for (ListNode head : lists) {
+            if (head != null) {
+                heap.offer(head);
+            }
+        }
+        // Set a dummy node to point to the head of the output linked list.
+        ListNode dummy = new ListNode(-1);
+        // Create a pointer to iterate through the combined linked list as 
+        // we add nodes to it.
+        ListNode curr = dummy;
+        while (!heap.isEmpty()) {
+            // Pop the node with the smallest value from the heap and add it 
+            // to the output linked list.
+            ListNode smallestNode = heap.poll();
+            curr.next = smallestNode;
+            curr = curr.next;
+            // Push the popped node's subsequent node to the heap.
+            if (smallestNode.next != null) {
+                heap.offer(smallestNode.next);
+            }
+        }
+        return dummy.next;
+    }
+}
