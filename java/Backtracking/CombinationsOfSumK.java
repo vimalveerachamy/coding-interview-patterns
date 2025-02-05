9bac83db10810467d375b60dcfb9a4d0fb39638a
Jer3myYu <jeremyyucareer@gmail.com>
2025-02-05 12:30:28 -0500

    fix: result to res

--- java/Backtracking/CombinationsOfSumK.java
+++ java/Backtracking/CombinationsOfSumK.java
@@ -3,16 +3,16 @@ import java.util.List;
 
 public class CombinationsOfSumK {
     public List<List<Integer>> combinationsOfSumK(int[] nums, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        dfs(new ArrayList<>(), 0, nums, target, result);
-        return result;
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(new ArrayList<>(), 0, nums, target, res);
+        return res;
     }
 
-    private void dfs(List<Integer> combination, int startIndex, int[] nums, int target, List<List<Integer>> result) {
+    private void dfs(List<Integer> combination, int startIndex, int[] nums, int target, List<List<Integer>> res) {
         // Termination condition: If the target is equal to 0, we found a combination 
         // that sums to 'k'.
         if (target == 0) {
-            result.add(new ArrayList<>(combination));
+            res.add(new ArrayList<>(combination));
             return;
         }
         // Termination condition: If the target is less than 0, no more valid 
@@ -25,7 +25,7 @@ public class CombinationsOfSumK {
             // Add the current number to create a new combination.
             combination.add(nums[i]);
             // Recursively explore all paths that branch from this new combination.
-            dfs(combination, i, nums, target - nums[i], result);
+            dfs(combination, i, nums, target - nums[i], res);
             // Backtrack by removing the number we just added.
             combination.remove(combination.size() - 1);
         }
