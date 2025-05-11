d475aedd850eaf3be1872ad0f3c68b86565a0a40
Mosharaf Hossain <mosharaf.hossain221@gmail.com>
2025-05-11 11:18:39 -0700

    Added type definition to LRUCache

--- csharp/Linked Lists/LRUCache.cs
+++ csharp/Linked Lists/LRUCache.cs
@@ -1,4 +1,28 @@
-﻿public class LRUCache
+using ds.DoublyLinkedListNode;
+
+/*
+	Definition of DoublyLinkedListNode:
+	class DoublyLinkedListNode
+	{
+		public DoublyLinkedListNode(int key, int val)
+		{
+			Key = key;
+			Val = val;
+			Prev = null;
+			Next = null;
+		}
+
+		public int Key { get; set; }
+
+		public int Val { get; set; }
+
+		public DoublyLinkedListNode Prev { get; set; }
+
+		public DoublyLinkedListNode Next { get; set; }
+	}
+*/
+
+public class LRUCache
 {
 	private readonly int _capacity;
 	private readonly Dictionary<int, DoublyLinkedListNode> _hashMap;
