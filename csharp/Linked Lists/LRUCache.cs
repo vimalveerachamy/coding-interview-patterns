21ad62986ef5fac50e64959a3678e86e09957311
Mosharaf Hossain <mosharaf.hossain221@gmail.com>
2025-05-12 07:32:02 -0700

    Uncomment DoublyLinkedListNode inside LRUCache.cs file since it is part of LRUCache

--- csharp/Linked Lists/LRUCache.cs
+++ csharp/Linked Lists/LRUCache.cs
@@ -1,26 +1,23 @@
 using ds.DoublyLinkedListNode;
 
-/*
-	Definition of DoublyLinkedListNode:
-	class DoublyLinkedListNode
+class DoublyLinkedListNode
+{
+	public DoublyLinkedListNode(int key, int val)
 	{
-		public DoublyLinkedListNode(int key, int val)
-		{
-			Key = key;
-			Val = val;
-			Prev = null;
-			Next = null;
-		}
-
-		public int Key { get; set; }
-
-		public int Val { get; set; }
-
-		public DoublyLinkedListNode Prev { get; set; }
-
-		public DoublyLinkedListNode Next { get; set; }
+		Key = key;
+		Val = val;
+		Prev = null;
+		Next = null;
 	}
-*/
+
+	public int Key { get; set; }
+
+	public int Val { get; set; }
+
+	public DoublyLinkedListNode Prev { get; set; }
+
+	public DoublyLinkedListNode Next { get; set; }
+}
 
 public class LRUCache
 {
