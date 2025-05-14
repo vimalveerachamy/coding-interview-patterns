919b5914c8824a266e2e0916ffd0b8159e18ae1e
mmhossain <mosharaf.hossain221@gmail.com>
2025-05-14 08:40:02 -0700

    Added C# solutions for Chapter 5: Sliding Windows

---
+++ csharp/Sliding Windows/LongestSubstringWithUniqueCharsOptimized.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public int LongestSubstringWithUniqueCharsOptimized(string s)
+    {
+        int maxLen = 0;
+        Dictionary<char, int> prevIndices = new Dictionary<char, int>();
+        int left = 0, right = 0;
+
+        while (right < s.Length)
+        {
+            // If a previous index of the current character is present
+            // in the current window, it's a duplicate character in the
+            // window. 
+            if (prevIndices.ContainsKey(s[right]) && prevIndices[s[right]] >= left)
+                // Shrink the window to exclude the previous occurrence
+                // of this character.
+                left = prevIndices[s[right]] + 1;
+
+            // Update 'maxLen' if the current window is larger.
+            maxLen = Math.Max(maxLen, right - left + 1);
+
+            prevIndices[s[right]] = right;
+
+            // Expand the window.
+            right++;
+        }
+
+        return maxLen;
+    }
+}
---
+++ csharp/Sliding Windows/SubstringAnagrams.cs
@@ -0,0 +1,46 @@
+﻿public class Solution
+{
+    public int SubstringAnagrams(string s, string t)
+    {
+        int lenS = s.Length, lenT = t.Length;
+
+        if (lenT > lenS)
+            return 0;
+
+        int count = 0;
+
+        int[] expectedFreqs = new int[26];
+        int[] windowFreqs = new int[26];
+
+        // Populate 'expectedFreqs' with the characters in string 't'.
+        foreach (char ch in t)
+            expectedFreqs[ch - 'a']++;
+
+        int left = 0, right = 0;
+
+        while (right < lenS)
+        {
+            // Add the character at the right pointer to 'windowFreqs'
+            // before sliding the window.
+            windowFreqs[s[right] - 'a']++;
+
+            // If the window has reached the expected fixed length, we 
+            // advance the left pointer as well as the right pointer to 
+            // slide the window.
+            if (right - left + 1 == lenT)
+            {
+                if (windowFreqs.SequenceEqual(expectedFreqs))
+                    count++;
+
+                // Remove the character at the left pointer from 
+                // 'windowFreqs' before advancing the left pointer.
+                windowFreqs[s[left] - 'a']--;
+                left++;
+            }
+
+            right++;
+        }
+
+        return count;
+    }
+}
