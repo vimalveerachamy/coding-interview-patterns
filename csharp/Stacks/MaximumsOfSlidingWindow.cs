f2c9bfcbb253e7aca36d33993811376f86ace33a
mmhossain <mosharaf.hossain221@gmail.com>
2025-05-21 07:45:58 -0700

    Added C# solutions for Chapter 7: Stacks

---
+++ csharp/Stacks/MaximumsOfSlidingWindow.cs
@@ -0,0 +1,40 @@
+﻿public class Solution
+{
+	public int[] MaximumsOfSlidingWindow(int[] nums, int k)
+	{
+		List<int> result = new List<int>();
+		LinkedList<(int num, int indx)> dq = new LinkedList<(int num, int indx)>();
+
+		int left = 0, right = 0;
+
+		while (right < nums.Length)
+		{
+			// 1) Ensure the values of the deque maintain a monotonic decreasing order
+			// by removing candidates <= the current candidate.
+			while (dq.Count > 0 && dq.Last().num <= nums[right])
+				dq.RemoveLast();
+
+			// 2) Add the current candidate.
+			dq.AddLast((nums[right], right));
+
+			// If the window is of length 'k', record the maximum of the window
+			if (right - left + 1 == k)
+			{
+				// 3)  Remove values whose indexes occur outside the window
+				if (dq.Count > 0 && dq.First().indx < left)
+					dq.RemoveFirst();
+
+				// The maximum value of this window is the leftmost value in the dq.
+				result.Add(dq.First().num);
+
+				// Slide the window by advancing both 'left' and 'right'. The right  
+				// pointer always gets advanced so we just need to advance 'left'.
+				left++;
+			}
+
+			right++;
+		}
+
+		return [.. result];
+	}
+}
---
+++ csharp/Stacks/NextLargestNumberToTheRight.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int[] NextLargestNumberToTheRight(int[] nums)
+    {
+        int[] res = new int[nums.Length];
+        Stack<int> stack = new Stack<int>();
+
+        // Find the next largest number of each element, starting with the 
+        // rightmost element.
+        for (int i = nums.Length - 1; i >= 0; i--)
+        {
+            // Pop values from the top of the stack until the current 
+            // value's next largest number is at the top.
+            while (stack.Count > 0 && stack.Peek() <= nums[i])
+                stack.Pop();
+
+            // Record the current value's next largest number, which is at 
+            // the top of the stack. If the stack is empty, record -1.
+            res[i] = stack.Count > 0 ? stack.Peek() : -1;
+
+            stack.Push(nums[i]);
+        }
+
+        return res;
+    }
+}
