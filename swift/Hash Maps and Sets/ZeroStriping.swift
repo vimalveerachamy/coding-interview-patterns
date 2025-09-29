ab8d9df355ef5b74d7df5d8415bbeae7b415a0c4
Vimal Veerachamhy <Vimal.Veerachamy@gmail.com>
2025-09-28 23:14:22 -0500

    HashMap and HashSet problems

--- swift/Hash Maps and Sets/ZeroStriping.swift
+++ swift/Hash Maps and Sets/ZeroStriping.swift
@@ -5,4 +5,27 @@
 //  Created by Vimal Veerachamy on 9/28/25.
 //
 
-import Foundation
+func setZeroes(_ matrix: inout [[Int]]) {
+    
+    var rows_set = Set<Int>()
+    var col_sets = Set<Int>()
+    
+    //Record all row and col whose values equals to 0
+    for r in 0..<matrix.count {
+        for c in 0..<matrix.first!.count {
+            if matrix[r][c] == 0 {
+                rows_set.insert(r)
+                col_sets.insert(c)
+            }
+        }
+    }
+    
+    //Scan and set all row and col values to 0 if any rows or columns have contain 0
+    for r in 0..<matrix.count {
+        for c in 0..<matrix.first!.count {
+            if rows_set.contains(r) || col_sets.contains(c) {
+                matrix[r][c] = 0
+            }
+        }
+    }
+}
--- swift/Hash Maps and Sets/verifySudokuBoard.swift
+++ swift/Hash Maps and Sets/verifySudokuBoard.swift
@@ -5,4 +5,34 @@
 //  Created by Vimal Veerachamy on 9/28/25.
 //
 
-import Foundation
+func isValidSudoku(_ board: [[Character]]) -> Bool {
+
+    let count = board.count
+    
+    //Initialize row, col and grid with Set
+    var row_sets = Array(repeating: Set<Character>(), count: count)
+    var col_sets = Array(repeating: Set<Character>(), count: count)
+    var grid_sets = Array(repeating: Set<Character>(), count: count)
+    
+    for r in 0..<board.count {
+        for c in 0..<board.count {
+            let char = board[r][c]
+            let idx = (r / 3) * 3 + (c / 3)
+            
+            //Skip if char == "."
+            if char == "." { continue }
+            
+            //Return invalid state if any row or col or grid contains duplicate
+            if row_sets[r].contains(char) { return false }
+            if col_sets[c].contains(char) { return false }
+            if grid_sets[idx].contains(char) { return false }
+            
+            row_sets[r].insert(char)
+            col_sets[c].insert(char)
+            grid_sets[idx].insert(char)
+            
+        }
+    }
+    
+    return true
+}
